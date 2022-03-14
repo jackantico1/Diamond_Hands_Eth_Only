2ab4d98e7fc98eaae3e00ab807a79109e40fd88c
Jack Antico <you@example.com>
2022-03-14 09:45:53 -0400

    Added some functions and event

--- Diamond_Hands_Eth_Only.sol
+++ Diamond_Hands_Eth_Only.sol
@@ -5,6 +5,7 @@ contract Diamond_Hands_Eth_Only {
 
   uint num_of_holds = 0;
   event print_num_of_holds(uint _num_of_holds);
+  event print_hold_id(uint _hold_id);
 
   struct Hold_Struct {
     uint hold_id;
@@ -20,7 +21,7 @@ contract Diamond_Hands_Eth_Only {
 
   mapping(uint => Hold_Struct) holds;
 
-  function make_eth_hold(uint _seconds_to_hold) public payable returns(uint) {
+  function make_eth_hold(uint _seconds_to_hold) public payable {
     Hold_Struct memory new_hold;
     new_hold.hold_id = num_of_holds;
     new_hold.addy = msg.sender;
@@ -28,7 +29,7 @@ contract Diamond_Hands_Eth_Only {
     new_hold.date_hold_created = block.timestamp;
     new_hold.wei_stored = msg.value;
     num_of_holds += 1;
-    return num_of_holds - 1;
+    emit print_hold_id(num_of_holds - 1);
   }
 
   function collect_hold(uint _hold_id) public {
@@ -36,9 +37,16 @@ contract Diamond_Hands_Eth_Only {
     payable(hold.addy).transfer(hold.wei_stored);
   }
 
-  function show_num_of_holds() public returns(uint) {
-      emit print_num_of_holds(num_of_holds);
-      return num_of_holds;
+  function see_time_left(uint _hold_id) public view returns(uint) {
+    return (holds[_hold_id].seconds_to_hold + holds[_hold_id].date_hold_created) - block.timestamp;
+  }
+
+  function return_num_of_holds() public view returns(uint) {
+    return num_of_holds;
+  }
+
+  function emit_event_num_of_holds() public {
+    emit print_num_of_holds(num_of_holds);
   }
 
 }
